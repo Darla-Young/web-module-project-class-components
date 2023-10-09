38916b028a031ad0a4f48c853e5802fc332fb941
Darla Young <89855627+Darla-Young@users.noreply.github.com>
2023-10-09 09:53:09 -0500

    onChange handler functional

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -2,39 +2,71 @@ import React from 'react'
 import Todo from './Todo'
 import Form from './Form'
 
+let newId = 0
+
 export default class App extends React.Component {
   // state
-    // keys: name=string, id=number, completed=bool
   constructor() {
     super()
     this.state = {
-      name: "",
-      id: 0,
-      completed: false
+      todos: [{
+        name: "",
+        id: 0,
+        completed: false
+      }]
     }
   }
+
   // handler functions
-    // onChange
     // onClick
-    // onSubmit
-  onChange = e => {
-    this.setState({...this.state, name: e.target.value})
-    console.log(e.target.value)
-  }
   onClick = e => {
-    this.setState({...this.state, completed: !this.state.completed})
-    console.log(e.target)
+    console.log(e.target.value)
+    this.setState(state => {
+      const todos = state.todos.map((todo, id) => {
+        if(id === e.target.id) {
+          todo.completed = !todo.completed
+          return todo
+        } else {return todo}
+      })
+      return {todos}
+    })
   }
+  
+    // onChange
+  onChange = e => {
+    console.log(e.target.value)
+    const volatile = this.state.todos.map(i => {
+      if(i.id == e.target.id){
+        return ({
+          ...i,
+          name: e.target.value
+        })
+      } else {return i}
+    })
+    this.setState({todos: volatile})
+  }
+  
+    // onSubmit
   onSubmit = e => {
-    this.todoArr.push({...this.state, id: this.state.id + 1, completed: false})
-    console.log(e.target)
+    e.preventDefault()
+    newId = Date.now()
+    this.setState(state => {
+      const todos = state.todos.concat({
+        name: "",
+        id: newId,
+        completed: false
+      })
+      return {todos}
+    })
+    console.log(this.state)
   }
+
   // render components
   render() {
     return (
       <div>
         <Todo onClick={this.onClick} data={this.state} />
-        <Form onChange={this.onChange} onSubmit={this.onSubmit} name={this.state.name} completed={this.state.completed} />
+        <Form id={newId} onChange={this.onChange} onSubmit={this.onSubmit} state={this.state} />
       </div>
     )
   }
--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -9,12 +9,12 @@ export default class Form extends React.Component {
     return (
       <form onSubmit={this.props.onSubmit} >
         <label> What needs done today? <br/>
-          <input name='todoName' type='text' onChange={this.props.onChange} value={this.props.name} />
+          <input id={this.props.id} name='todoName' type='text' onChange={this.props.onChange} value={this.props.state.todos.find(i => i.id === this.props.id).name} />
         </label><br/><br/>
         <button>
           Add Todo
         </button>
-        <button>
+        <button style={{display: "none"}} >
           Hide Completed
         </button>
       </form>
